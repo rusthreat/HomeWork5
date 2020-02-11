8b4eaae2d3e6a8fa33d9adeec7f7ff1c2dd582dd
zrf <lexusrus@mail.ru>
2020-02-11 19:16:20 +0300

    Задание 1 - решено

--- Program.cs
+++ Program.cs
@@ -13,26 +13,50 @@ namespace Example_005
 
         #region Задание 1 (матрицы из 4-го модуля)          
         // Задание 1.
-        // Воспользовавшись решением задания 3 четвертого модуля
+        // Используется решение: 3-го задания четвертого модуля
         // 1.1. Создать метод, принимающий число и матрицу, возвращающий матрицу умноженную на число
-        
+        // 1.2. Создать метод, принимающий две матрицу, возвращающий их сумму
+        // 1.3. Создать метод, принимающий две матрицу, возвращающий их произведение
+        //
+
+        /// <summary>
+        /// Метод, генерирующий матрицу заданного размера
+        /// </summary>
+        static int[][] GetMatrix(int n, int m)
+        {
+            int[][] Matrix = new int[n][];
+
+            // формирование начальной матрицы
+            for (int j = 0; j < n; j++)
+            {
+                Matrix[j] = new int[m];
+
+                for (int i = 0; i < m; i++)
+                {
+                    Matrix[j][i] = random.Next(1, 10);
+                }
+            }
+
+            return Matrix;
+        }
+
         /// <summary>
         /// Метод, принимающий число и матрицу, возвращающий результат их умножения
         /// </summary>
         static int[][] MultMatrixByNumber(int a, int[][] Array)
         {
-            // 
-            int n = Array[].GetLength();
-            int m = Array[][].GetLength();
-            
+            // Определение размера входящего массива
+            int n = Array.Length;
+            int m = Array[0].Length;
+
             // объявление итоговой матрицы
             int[][] Total = new int[n][];
             // 
-            
+
             for (int j = 0; j < n; j++)
-            { 
+            {
                 Total[j] = new int[m];
-                        
+
                 for (int i = 0; i < m; i++)
                 {
                     Total[j][i] = Array[j][i] * a;
@@ -41,10 +65,95 @@ namespace Example_005
 
             return Total;
         }
-            
-        // 1.2. Создать метод, принимающий две матрицу, возвращающий их сумму
-        // 1.3. Создать метод, принимающий две матрицу, возвращающий их произведение
-        //
+
+        /// <summary>
+        /// Метод, принимающий 2 матрицы и возвращающий результат их сложения или вычитания
+        /// flag = 0 - сложение
+        /// flag = 1 - вычитание
+        /// </summary>
+        static int[][] MatrixAddition(int flag, int[][] Array1, int[][] Array2)
+        {
+            // Определение размера входящей матрицы
+            int n = Array1.Length;
+            int m = Array1[0].Length;
+
+            // объявление итоговой матрицы
+            int[][] Total = new int[n][];
+
+            for (int j = 0; j < n; j++)
+            {
+                Total[j] = new int[m];
+
+                for (int i = 0; i < m; i++)
+                {
+                    if (flag == 0)
+                    {
+                        Total[j][i] = Array1[j][i] + Array2[j][i];
+                    }
+                    else 
+                    {
+                        Total[j][i] = Array1[j][i] - Array2[j][i];
+                    }
+                }
+            }
+            return Total;
+        }
+
+        /// <summary>
+        /// Метод, принимающий 2 матрицы, возвращающий результат их умножения
+        /// </summary>
+        static int[][] MatrixMultiplication(int[][] Array1, int[][] Array2)
+        {
+            // Определение размера входящих матриц
+            int n1 = Array1.Length;
+            int m1 = Array1[0].Length;
+            int n2 = Array2.Length;
+            int m2 = Array2[0].Length;
+            // объявление итоговой матрицы
+            int[][] Total = new int[n1][];
+
+            // Расчет умножения
+            for (int j = 0; j < n1; j++)
+            {
+                Total[j] = new int[m2];
+
+                for (int i = 0; i < m2; i++)
+                {
+                    int sum = 0;
+
+                    for (int k = 0; k < m1; k++)
+                    {
+                        sum = sum + (Array1[j][k] * Array2[k][i]);
+                    }
+
+                    Total[j][i] = sum;
+                }
+            }
+
+            return Total;
+        }
+
+        /// <summary>
+        /// Метод, выводящий переданный массив на экран
+        /// </summary>
+        static void PrintMatrix(int[][] Array)
+        {
+            // Определение размера входящего массива
+            int n = Array.Length;
+            int m = Array[0].Length;
+
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write("\n|");
+
+                for (int i = 0; i < m; i++)
+                {
+                    Console.Write($"{Array[j][i],4:0}");
+                }
+
+                Console.Write("  |");
+            }
+        }
 
         /// <summary>
         /// Задание 1. Метод для выполнения арифметических операций с матрицами
@@ -90,23 +199,10 @@ namespace Example_005
                     int m = Convert.ToInt32(Console.ReadLine());
 
                     // объявление начальной матрицы
-                    int[][] Array = new int[n][];
+                    int[][] Array = GetMatrix(n, m);
 
                     Console.WriteLine("Укажите число-множитель: ");
                     int a = Convert.ToInt32(Console.ReadLine());
-
-                    // формирование начальной матрицы
-                    for (int j = 0; j < n; j++)
-                    { 
-                        Array[j] = new int[m];
-                        //Total[j] = new int[m];
-                        
-                        for (int i = 0; i < m; i++)
-                        {
-                            Array[j][i] = random.Next(1,10);
-                            //Total[j][i] = Array[j][i] * a;
-                        }
-                    }
 
                     // обращение к методу, выполняющему умножение
                     int[][] Total = MultMatrixByNumber(a, Array);
@@ -178,8 +274,8 @@ namespace Example_005
                 
                 else if (item == 2)
                 {
-                    int n = 0;
-                    int m = 0;
+                    int n;
+                    int m;
                     
                     while (true)
                     {
@@ -204,36 +300,16 @@ namespace Example_005
                     }   
                     
                     // объявление 1-й матрицы
-                    int[][] Array1 = new int[n][];
+                    int[][] Array1 = GetMatrix(n, m);
 
                     // объявление 2-й матрицы
-                    int[][] Array2 = new int[n][];
+                    int[][] Array2 = GetMatrix(n, m);
 
-                    // объявление 1-й итоговой матрицы
-                    int[][] Total1 = new int[n][];
+                    // расчет 1-й итоговой матрицы
+                    int[][] Total1 = MatrixAddition(0, Array1, Array2);
 
                     // объявление 2-й итоговой матрицы
-                    int[][] Total2 = new int[n][];
-
-                    // формирование матриц
-                    for (int j = 0; j < n; j++)
-                    { 
-                        Array1[j] = new int[m];
-                        Array2[j] = new int[m];
-
-                        Total1[j] = new int[m];
-                        Total2[j] = new int[m];
-
-                        for (int i = 0; i < m; i++)
-                        {
-                            Array1[j][i] = random.Next(1,10);
-                            Array2[j][i] = random.Next(1,10);
-                            
-                            // Расчет значений итоговых матриц    
-                            Total1[j][i] = Array1[j][i] + Array2[j][i];
-                            Total2[j][i] = Array1[j][i] - Array2[j][i];
-                        }
-                    }
+                    int[][] Total2 = MatrixAddition(1, Array1, Array2);
 
                     // вывод результата
                     // поиск строки, на которой будут выводится знаки
@@ -361,10 +437,10 @@ namespace Example_005
                 
                 else if (item == 3)
                 {
-                    int n1 = 0;
-                    int n2 = 0;
-                    int m1 = 0;
-                    int m2 = 0;
+                    int n1;
+                    int n2;
+                    int m1;
+                    int m2;
                     
                     while (true)
                     {
@@ -394,74 +470,27 @@ namespace Example_005
                         }
                     }   
                     
-                    // объявление 1-й матрицы
-                    int[][] Array1 = new int[n1][];
+                    // получение 1-й матрицы
+                    int[][] Array1 = GetMatrix(n1, m1);
 
-                    // объявление 2-й матрицы
-                    int[][] Array2 = new int[n2][];
+                    // получение 2-й матрицы
+                    int[][] Array2 = GetMatrix(n2, m2);
 
                     // объявление итоговой матрицы
-                    int[][] Total = new int[n1][];
-
-                    // формирование и вывод 1-й матрицы
-                    Console.WriteLine("\nМатрица 1:");
-                    for (int j = 0; j < n1; j++)
-                    { 
-                        Array1[j] = new int[m1];
-                        
-                        Console.Write("\n|");
-
-                        for (int i = 0; i < m1; i++)
-                        {
-                            Array1[j][i] = random.Next(1,10);
-                            Console.Write($"{Array1[j][i],3:0}");
-                        }
-
-                        Console.Write("  |");
-                    }
-
-                    // формирование и вывод 2-й матрицы
-                    Console.WriteLine("\n\nМатрица 2:");
-                    for (int j = 0; j < n2; j++)
-                    { 
-                        Array2[j] = new int[m2];
-                        
-                        Console.Write("\n|");
-
-                        for (int i = 0; i < m2; i++)
-                        {
-                            Array2[j][i] = random.Next(1,10);
-                            Console.Write($"{Array2[j][i],3:0}");
-                        }
-
-                        Console.Write("  |");
-                    }
+                    int[][] Total = MatrixMultiplication(Array1, Array2);
 
                     // Вывод результата
-                    // Расчет умножения
+                    Console.WriteLine("\nМатрица 1:");
+                    PrintMatrix(Array1);
+
+                    Console.WriteLine("\n\nМатрица 2:");
+                    PrintMatrix(Array2);
+
                     Console.WriteLine("\n\nИтог умножения матриц:");
-                    for (int j = 0; j < n1; j++)
-                    { 
-                        Total[j] = new int[m2];
-                        Console.Write("\n|");
-
-                        for (int i = 0; i < m2; i++)
-                        {
-                            int sum = 0;
-
-                            for (int k = 0; k < m1; k++)
-                            { 
-                                sum = sum + (Array1[j][k] * Array2[k][i]);
-                            }
-                            
-                            Total[j][i] = sum;
-                            Console.Write($"{Total[j][i],4:0}");
-                        }
-                        Console.Write("  |");
-                    } 
+                    PrintMatrix(Total);
                 }
                 #endregion
-                
+
                 else
                 {
                     Console.WriteLine("Укажите верное значение");
